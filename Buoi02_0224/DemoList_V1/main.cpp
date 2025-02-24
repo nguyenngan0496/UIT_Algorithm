dc0389a1157fb11f96e1094613d6ae0e16000a39
nguyenngan0496 <memmem0496@gmail.com>
2025-02-24 21:55:05 +0700

    Buoi 2

--- Buoi02_0224/DemoList_V1/main.cpp
+++ Buoi02_0224/DemoList_V1/main.cpp
@@ -1,104 +1,128 @@
 #include <iostream>
-
 using namespace std;
 
-// Bước 1: Tạo cấu trúc node
-struct Node
-{
-    int data;
-    Node* pNext; // Phải là kiểu node* thì mới lưu được địa chỉ
+// Buoc 01: Tạo cấu trúc Node
+struct Node {
+  int data;
+  Node *pNext; // lưu trữ địa chỉ của Node phía sau
 };
 
-// Bước 2: Cấp phát node để lưu trữ dữ liệu
-Node* initNode(int value)
-{
-    Node* p = new Node;
-    p->data = value;
-    p->pNext = NULL;
-    return p;
+// Bước 2: Khởi tạo Node để lưu giá trị cần lưu.
+Node* initNode(int value) {
+  Node* p = new Node;
+  p->data = value;
+  p->pNext = NULL; // chứa lưu trữ địa chỉ Node tiếp theo, set = NULL
+  return p;
 }
 
 // Bước 3: Tạo cấu trúc danh sách
-struct List
-{
-    Node* pHead;
-    Node* pTail;
+struct List {
+  Node* pHead;
+  Node* pTail;
 };
 
-// Bước 4: Hàm khởi tạo danh sách
-void initList(List& l)
-{
-    l.pHead = NULL;
-    l.pTail = NULL;
+// Bước 4: Khởi tạo danh sách
+void initList(List& l) {
+  l.pHead = NULL;
+  l.pTail = NULL;
 }
 
-// Thêm node vào đầu danh sách
-void addHead(List& l, Node* p)
-{
-    if (l.pHead == NULL)
-    {
-        l.pHead = p;
-        l.pTail = p;
-    }
-    else
-    {
-        p->pNext = l.pHead;
-        l.pHead = p;
-    }
+void addHead(List& l, Node* p) {
+  if (l.pHead == NULL) {
+    l.pHead = p;
+    l.pTail = p;
+  } else {
+    p->pNext = l.pHead;
+    l.pHead = p;
+  }
 }
 
-// Thêm node vào cuối danh sách
-void addTail(List& l, Node* p)
-{
-    if (l.pHead == NULL)
-    {
-        l.pHead = p;
-        l.pTail = p;
-    }
-    else
-    {
-        l.pTail->pNext = p;
-        l.pTail = p;
-    }
+void addTail(List& l, Node* p) {
+  if (l.pHead == NULL) {
+    l.pHead = p;
+    l.pTail = p;
+  } else {
+    l.pTail->pNext = p;
+    l.pTail = p;
+  }
 }
 
-// Duyệt danh sách và in dữ liệu bằng vòng for
-void printListFor(List l)
-{
-    for (Node* p = l.pHead; p != NULL; p = p->pNext)
-    {
-        cout << p->data << "  ";
-    }
+void printListFor(List l) {
+  for (Node* p = l.pHead; p != NULL; p = p->pNext) {
+    cout << p->data << " ";
+  }
     cout << endl << endl;
 }
 
-// Duyệt danh sách và in dữ liệu bằng vòng while (sửa lỗi)
-void printListWh(List l)
-{
-    Node* p = l.pHead;
-    while (p != NULL)
-    {
-        cout << p->data << "  ";
-        p = p->pNext; // Cập nhật con trỏ để duyệt qua danh sách
-    }
-    cout << endl << endl;
+void printListWhile(List l) {
+  Node* p = l.pHead;
+  while (p != NULL) {
+    cout << p->data << " ";
+    p = p->pNext;
+  }
+  cout << endl << endl;
 }
 
-int main()
-{
-    Node* p1 = initNode(10);
-    Node* p2 = initNode(79);
-    Node* p3 = initNode(39);
+/* Yêu cầu: Viết hàm tìm kiếm một giá trị có trong List hay không ?
+Input:
++ l: List
++ value: int
+Output:
++  return bool
++
+ */
+bool timGiaTri(List l, int value) {
+  for (Node* p = l.pHead; p != NULL; p = p->pNext) {
+    if (p->data == value) {
+      return true;
+    }
+  }
+  return false;
+}
 
-    List l;
-    initList(l); // Danh sách rỗng
+/* Yêu cầu: Viết hàm đếm giá trị chẵn có trong List ?
+Input:
++ l: List
+Output:
++  return int
+ */
+int demGiaTri(List l, int value) {
+  for (Node* p = l.pHead; p != NULL; p = p->pNext) {
+    // TODO: xử lý
+  }
+  return 0;
+}
 
-    addHead(l, p1);
-    addHead(l, p2);
-    addTail(l, p3);
 
-    printListFor(l);
-    printListWh(l);
+int main() {
+  Node* p1 = initNode(10);
+  Node* p2 = initNode(79);
+  Node* p3 = initNode(39);
 
-    return 0;
+  List l;
+  initList(l); //Danh sách rỗng
+
+  addHead(l,p1);
+  addHead(l,p2);
+
+  addTail(l,p3);
+
+  cout << "Test: In danh sách:" << endl;
+  //79 10 39
+  printListFor(l);
+  printListWhile(l);
+  cout << "Test: Tim(10)_(1): " << timGiaTri(l,10) << endl;
+  cout << "Test: Tim(29)_(0): " << timGiaTri(l,29) << endl;
+  // Về nhà tìm giá trị nút kế cuối
+  // Làm bài tập về nhà câu 1,2,3
+
+//  Node* p = new Node;
+//  p->data = 10;
+//  p->pNext = NULL; // chưa lưu trữ địa chỉ Node tiếp theo, set = NULL
+//
+//  Node* p1 = new Node;
+//  p1->data = 79;
+//  p1->pNext = NULL;
+
+  return 0;
 }
