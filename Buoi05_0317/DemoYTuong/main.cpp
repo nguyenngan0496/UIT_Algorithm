7259b0bcd852e7dd206e102f08b81ed8f9e28ce5
nguyenngan0496 <memmem0496@gmail.com>
2025-03-22 23:28:00 +0700

    Buoi 5

---
+++ Buoi05_0317/DemoYTuong/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+//B1: tao cau truc node
+struct Node
+{
+    int data;
+    Node* pNext;
+};
+
+//B2: Khoi tao node - cap phat dong
+Node* initNode(int value)
+{
+    Node* p = new Node;
+    p->data = value;
+    p->pNext = NULL;
+
+    return p;
+}
+
+//B3: Dinh nghia stack
+struct Stack
+{
+    Node* pTop;
+};
+
+//B4: Khoi tao stack
+void initStack(Stack& s)
+{
+    s.pTop = NULL;
+}
+
+void push(Stack& s, Node* p)
+{
+    if(s.pTop == NULL)
+    {
+        s.pTop = p;
+    }
+    else
+    {
+        p->pNext = s.pTop;
+        s.pTop = p;
+    }
+}
+
+void printStack(Stack s)
+{
+    cout << "Stack = Top << ";
+
+    for(Node* p = s.pTop; p != NULL; p = p->pNext)
+    {
+        cout << p->data << "    ";
+    }
+    cout << " >>" << endl;
+}
+
+void pop(Stack& s)
+{
+    if(s.pTop == NULL)
+        return;
+
+    Node* p = s.pTop;
+    s.pTop = s.pTop->pNext;
+    delete p;
+}
+
+int getValue(Stack s)
+{
+    if(s.pTop != NULL)
+        return s.pTop->data;
+
+}
+
+void convert(int n, Stack& s)
+{
+    for(; n != 0; n = n/2)
+    {
+        push(s, initNode(n%2));
+    }
+}
+
+int main()
+{
+    Stack s;
+    initStack(s);
+
+    convert(13, s);
+    printStack(s);
+
+    return 0;
+}
